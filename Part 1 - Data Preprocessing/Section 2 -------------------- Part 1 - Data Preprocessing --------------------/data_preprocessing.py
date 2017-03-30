85946eb39ea0f5250a39bf0bff171206eb05a9fe
marifw <muhammad.arif@id.streamintelligence.com>
2017-03-30 15:13:24 +0700

    finish doing ata preprocessing

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import Imputer
 
 dataset = pd.read_csv('../../datasets/Data_Preprocessing/Data.csv')
 print(dataset.values)
@@ -10,3 +11,8 @@ print(X)
 
 Y = dataset.iloc[:, 3].values
 print(Y)
+
+# data preprocessing
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
